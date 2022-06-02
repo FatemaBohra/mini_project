06aeca1344252cfe82944bd3fd54ad2b7ac6024f
Fatema Bohra <fatema.bohra2003@gmail.com>
2022-06-02 17:56:06 +0530

    Add the miniProject file

---
+++ miniProject.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class miniProject {
+    public static void main(String[] args) {
+        // Mini project
+        Scanner userInput = new Scanner(System.in);
+        int myNumber = (int) (Math.random() * 100);
+        int userNumber;
+
+        do {
+            System.out.print("Please guess a number (0-100): ");
+            userNumber = userInput.nextInt();
+
+            if (userNumber < 0) {
+                break;
+            } else if (userNumber == myNumber) {
+                System.out.println("Yayyy...correct number!");
+                break;
+            } else if (userNumber > myNumber) {
+                System.out.println("Your number is large");
+
+            } else {
+                System.out.println("Your number is small");
+            }
+        } while (userNumber >= 0);
+
+        System.out.print("My number was: ");
+        System.out.println(myNumber);
+    }
+}
